974fcdd960d83c816c75871d66ddf42610244be2
Varun <varunlakshmananap@gmail.com>
2023-10-17 21:24:15 +0200

    fix: deps and tw v3 support

--- src/helpers/server.ts
+++ src/helpers/server.ts
@@ -1,8 +1,6 @@
 import { resolve } from 'path';
 import http from 'http';
 import connect from 'connect';
-import openUrl from 'open';
-import getPort from 'get-port';
 import sirv from 'sirv';
 import { version } from '../../package.json';
 import { Metrics } from '../types/metrics';
@@ -12,12 +10,12 @@ export async function startServer(output: {
   metrics: Metrics;
   categories: ClassnameInfo[];
 }) {
-  const port = await getPort();
+  const getPort = await import('get-port');
+  const port = await getPort.default();
   const app = connect();
   const url = `http://localhost:${port}`;
 
-  // @ts-ignore
-  app.use('/api', async function fooMiddleware(req, res) {
+  app.use('/api', async function fooMiddleware(_, res) {
     res.write(JSON.stringify(output));
     res.statusCode = 500;
     return res.end();
@@ -27,7 +25,8 @@ export async function startServer(output: {
 
   http.createServer(app).listen(port);
 
-  openUrl(url);
+  const openUrl = await import('open')
+  openUrl.default(url);
 
   console.log(`TailwindCSS Analysis ${version} report open at ${url}`);
 
